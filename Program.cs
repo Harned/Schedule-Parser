2ae603823bf899b06a8570d9797428a2386eb7d9
barmem <igor.neim7@gmail.com>
2022-01-29 02:25:11 +0600

    nex method

--- Program.cs
+++ Program.cs
@@ -14,15 +14,14 @@ public class Program
         web.OverrideEncoding = Encoding.GetEncoding("windows-1251");
 		var doc = web.Load("http://fkn.omsu.ru/academics/Schedule/schedule2_1.htm");
 		// Using LINQ to parse HTML table smartly 
-		var HTMLTableTRList = from table in doc.DocumentNode.SelectNodes("//table").Cast<HtmlNode>()
-            from row in table.SelectNodes("tr").Cast<HtmlNode>()
-            from cell in row.SelectNodes("th|td").Cast<HtmlNode>()
-            select new {/*rows = table.sdf,*/ Cell_Text = cell.InnerText, node =cell.Attributes.Count };
-
-		// now showing output of parsed HTML table
-		foreach(var cell in HTMLTableTRList) 
-		{
-    		Console.WriteLine("{0}", /*cell.Cell_Text,*/ cell.node);
-		}
+		foreach (HtmlNode table in doc.DocumentNode.SelectNodes("//table")) {
+            // Console.WriteLine("Found: " + table.Id);
+        foreach (HtmlNode row in table.SelectNodes("tr")) {
+            Console.WriteLine("row");
+        foreach (HtmlNode cell in row.SelectNodes("th|td")) {
+            Console.WriteLine("cell: " + cell.InnerText);
+        }
+    }
+}
 	}
 }
